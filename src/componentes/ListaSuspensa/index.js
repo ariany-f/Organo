779bb325198e10d7b9b0c93c33a2fb97339c7c6a
ariany-f <ariany_f@hotmail.com>
2023-10-13 20:51:28 -0300

    Times

---
+++ src/componentes/ListaSuspensa/index.js
@@ -0,0 +1,21 @@
+import './ListaSuspensa.css'
+
+const ListaSuspensa = (props) => {
+
+    const aoDigitado = (evento) => {
+        props.aoAlterado(evento.target.value)
+    }
+
+    return (
+        <div className="lista-suspensa">
+            <label>{props.label}</label>
+            <select value={props.valor} onChange={aoDigitado} required={props.obrigatorio}>
+                {props.itens.map(item => {
+                    return <option key={item}>{item}</option>
+                })}
+            </select>
+        </div>
+    )
+}
+
+export default ListaSuspensa
